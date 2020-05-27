5b35f3938ed89e6df250a63734776316227f0997
Fabio Mencoboni <Fabio.Mencoboni@gmail.com>
2020-05-27 08:20:13 -0400

    a few pieces are laying here and there

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # shibboleth
-Simple, pure-Rust word2vec training of word vectors.
+A simple, pure-Rust implementation of word2vec embeddings with word stemming and negative sampling.
+
+#### Getting Data In
+Shibboleth can use training corpora provided in an sqlite file matching this schema:
+```
+CREATE TABLE documents (id PRIMARY KEY, text);
+```
+A popular resource for training purposes is Wikipedia. The script below will download and unzip such a sqlite file with just over 5 million documents. For the wiki license see [here](https://en.wikipedia.org/wiki/Wikipedia:Reusing_Wikipedia_content).
+```
+$ wget -O wiki.db.gz https://dl.fbaipublicfiles.com/drqa/docs.db.gz && gunzip wiki.db.gz
+```
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 
 use std::collections::HashMap;
 use rand::Rng;
+use zoea::nlp;
+use sqlite as db;
+use std::fs::File;
+use std::io::{Write};//, BufReader, BufRead, Error};
+
+
 
 #[derive(Debug)]
 struct Nexus {
@@ -21,19 +27,49 @@ struct Encoder<'a> {
     vec_size: usize,                        // dimensionality of word embeddings
     epochs: usize,                          // # of training ephocs completed
     vocab: HashMap<&'a str, u32>,              // frequency of vocab occurence
-    output: HashMap<&'a str, u32>,
+    word_idx: HashMap<usize, &'a str>,      // index words so you can use random sampling
     weights: HashMap<(&'a str, usize), Nexus>, // weights
-    hidden: HashMap<usize, f32>,
-    errors: HashMap<usize, f32>
 } 
 
 impl Encoder<'_> {
-    fn feedforward(&mut self, word: &str) {
+    fn feedforward(&mut self, input: &str, output: &str) {
+
         for i in 0..self.vec_size {
-            &self.hidden.insert(i, 1f32);
+            println!("");
         }
     }
 }
+
+
+pub fn build_vocab_from_db(db_file: &str, out_file: &str, vocab_size: usize) {
+    // build vocabulary from database db_file
+    // keep the top vocab_size terms and save to out_file
+    let mut counts: HashMap<&str, usize> = HashMap::new();
+    let conn = db::open(&db_file).unwrap();
+    conn.iterate("SELECT text FROM documents LIMIT 30",	|pairs| {
+		for &(_, value) in pairs.iter() { // _ = column
+			// build a list then use it below, as you can't borrow twice
+			let document: String = value.unwrap().to_string();
+            let tokens = nlp::text_tokens(&document);
+            for tok in tokens {
+                println!("{}", tok);
+            }
+        } true
+    }).unwrap();
+}
+/**
+ let conn = db::open(&db_name).unwrap();
+self.conn
+			.iterate(expr,
+				|pairs| {
+				for &(_, value) in pairs.iter() { // _ = column
+					// build a list then use it below, as you can't borrow twice
+					let firstword: String = value.unwrap().to_string();
+					set_firstwords.insert(firstword);
+
+				} true
+		   }).unwrap();
+ */
 fn main() {
 
     let mut weights: HashMap<(&str, usize), Nexus> = HashMap::new();
@@ -42,6 +78,16 @@ fn main() {
         weights.insert( (&word, i), Nexus::new());
     }
 
-    
+    let sentence = String::from("Today I walked slowly to the garden in San Diego.");
+    let tokens = nlp::text_tokens(&sentence);
+    for tok in tokens {
+        println!("{}", tok);
+    }
+    build_vocab_from_db("wiki.db","TBD",20);
+
+    let path = "lines.txt";
+
+    let mut output = File::create(path).unwrap();
+    write!(output, "Rust\n💖\nFun").unwrap();
     println!("Hello, world!");
 }
