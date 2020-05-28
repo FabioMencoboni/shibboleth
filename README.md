6d3ca0c14906d47c6ca21f6c66334357fa425626
Fabio Mencoboni <Fabio.Mencoboni@gmail.com>
2020-05-27 20:55:52 -0400

    prediction works

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # shibboleth
-A simple, pure-Rust implementation of word2vec embeddings with word stemming and negative sampling.
 
+### A simple, pure-Rust implementation of word2vec with stemming and negative sampling. With *Shibboleth* you can easily
+- **Build** a corpus vocabulary.
+- **Train** word vectors
+- **Find** words based on vector distance.
+
+#### Automatic text tokenization
+
+```
+let tokens = shibboleth::tokenize("Totally! I love cupcakes!");
+assert_eq!(tokens[0], "total");
+assert_eq!(tokens[3], "cupcak");
+```
 #### Getting Data In
+
 Shibboleth can use training corpora provided in an sqlite file matching this schema:
 ```
 CREATE TABLE documents (id PRIMARY KEY, text);
@@ -9,4 +21,11 @@ CREATE TABLE documents (id PRIMARY KEY, text);
 A popular resource for training purposes is Wikipedia. The script below will download and unzip such a sqlite file with just over 5 million documents. For the wiki license see [here](https://en.wikipedia.org/wiki/Wikipedia:Reusing_Wikipedia_content).
 ```
 $ wget -O wiki.db.gz https://dl.fbaipublicfiles.com/drqa/docs.db.gz && gunzip wiki.db.gz
+```
+#### Building Vocabulary
+
+This example takes the *wiki.db* file downloaded above, runs through the first 1,000,000 documents, stems them, and builds a vocabulary of the 25,000 most common words. The output will be saved to WikiVocab.txt
+```
+use shibboleth;
+shibboleth::build_vocab_from_db("wiki.db", "WikiVocab25k.txt", 1000000, 25000);
 ```
--- src/main.rs
+++ src/main.rs
@@ -3,59 +3,152 @@ use std::collections::HashMap;
 use rand::Rng;
 use zoea::nlp;
 use sqlite as db;
-use std::fs::File;
-use std::io::{Write};//, BufReader, BufRead, Error};
+use std::{cmp, fs::File, io::{self, BufReader,Write}};
+use std::io::prelude::*; // needed to have File.lines() work
+//use std::;//, BufReader, BufRead, Error};
 
 
+pub fn tokenize(text: &str) -> Vec<String> {
+    let tokens = nlp::text_tokens(text);
+    tokens
+}
 
-#[derive(Debug)]
+
+#[derive(Debug, Copy, Clone)]
 struct Nexus {
-    forward: f32,
-    backward: f32,
+    w_in_to_hidden: f32,
+    w_hidden_to_out: f32,
 }
 
-impl Nexus {
-    fn new() -> Nexus {
-        let mut rng = rand::thread_rng();
-        let mut forward = -0.01f32 + 0.02f32*rng.gen::<f32>();
-        let mut backward = -0.01f32 + 0.02f32*rng.gen::<f32>();
-        Nexus{forward:forward, backward:backward}
-    }
-}
 
-struct Encoder<'a> {
+struct Encoder {
     vec_size: usize,                        // dimensionality of word embeddings
     epochs: usize,                          // # of training ephocs completed
-    vocab: HashMap<&'a str, u32>,              // frequency of vocab occurence
-    word_idx: HashMap<usize, &'a str>,      // index words so you can use random sampling
-    weights: HashMap<(&'a str, usize), Nexus>, // weights
+    vocab: HashMap<String, (usize, usize)>,              // word -> (index, frequency)
+    weights: HashMap<(usize, usize), Nexus> // weights
 } 
 
-impl Encoder<'_> {
-    fn feedforward(&mut self, input: &str, output: &str) {
-
-        for i in 0..self.vec_size {
-            println!("");
+impl Encoder {
+    pub fn new(vec_size: usize, vocab_file: &str) -> Encoder {
+        // generate and return a new Encoder using a specified vector size and a vocabulary file
+        println!("Initializing a new encoder with {}-element word vectors using {}", &vec_size, &vocab_file);
+        let vocab = load_vocab(vocab_file);
+        let mut rng = rand::thread_rng();
+        let mut w_in_to_hidden: f32;// = -0.01f32 + 0.02f32*rng.gen::<f32>();
+        let mut w_hidden_to_out: f32;// = -0.01f32 + 0.02f32*rng.gen::<f32>();
+        let mut weights: HashMap<(usize, usize), Nexus> = HashMap::new();
+        for (key, (index, count)) in vocab.iter() {
+            for j in 0..vec_size {
+                w_in_to_hidden = -0.01f32 + 0.02f32*rng.gen::<f32>();
+                w_hidden_to_out = -0.01f32 + 0.02f32*rng.gen::<f32>();
+                let nex = Nexus{w_in_to_hidden: w_in_to_hidden, w_hidden_to_out:w_hidden_to_out};
+                weights.insert((*index, j), nex);
+            }
         }
+        let enc = Encoder{vec_size: vec_size, epochs: 0, vocab: vocab.clone(), weights: weights};
+        enc // return the encoder object
     }
+
+    pub fn predict(&self, input: &str, output: &str) -> Option<f32> {
+        let null_32: Option<f32> = None; 
+        let input_idx: usize = match self.vocab.get(input){
+            Some(val) => val.0,
+            None => return null_32
+        };
+        let output_idx: usize = match self.vocab.get(output){
+            Some(val) => val.0,
+            None => return null_32
+        };
+        let mut z: f32 = 0f32;
+        let mut win: f32;
+        let mut wout: f32;
+        for j in 0..self.vec_size {
+            win = match self.weights.get(&(input_idx, j)) {
+                Some(&val) => val.w_hidden_to_out,
+                None => 0f32
+            };
+            wout = match self.weights.get(&(output_idx, j)) {
+                Some(&val) => val.w_hidden_to_out,
+                None => 0f32
+            };
+            z = z + (win * wout);
+        }
+        Some(z)
+    }
+
 }
 
 
-pub fn build_vocab_from_db(db_file: &str, out_file: &str, vocab_size: usize) {
+
+fn load_vocab(vocab_file: &str) -> HashMap<String, (usize, usize)> {
+    let mut word = String::new();
+    let mut count: usize = 0;
+    let mut position: usize = 0;
+    let mut vocab: HashMap<String, (usize, usize)> = HashMap::new();
+    let mut index: usize = 0;
+
+    let f = File::open(vocab_file).unwrap();
+    let f = BufReader::new(f);
+
+    for line in f.lines() {
+        let line = line.unwrap();
+        let line = line.split_whitespace();
+        for x in line {
+            position  = position + 1;
+            if position % 2 == 1 {
+                word = x.to_string();
+            } else {
+                count = x.parse::<usize>().unwrap();
+            }
+        }
+        vocab.insert(word.to_string(), (index, count));
+        index = index + 1;
+    }
+    vocab
+}
+
+pub fn build_vocab_from_db(db_file: &str, vocab_file: &str, n_docs: usize, vocab_size: usize) {
     // build vocabulary from database db_file
-    // keep the top vocab_size terms and save to out_file
-    let mut counts: HashMap<&str, usize> = HashMap::new();
+    // keep the top vocab_size terms and save to vocab_file
+    let mut vocab: HashMap<String, usize> = HashMap::new();
+    let mut n_doc: usize = 0;
     let conn = db::open(&db_file).unwrap();
-    conn.iterate("SELECT text FROM documents LIMIT 30",	|pairs| {
+    conn.iterate(format!("SELECT text FROM documents LIMIT {}", n_docs),	|pairs| {
 		for &(_, value) in pairs.iter() { // _ = column
 			// build a list then use it below, as you can't borrow twice
-			let document: String = value.unwrap().to_string();
-            let tokens = nlp::text_tokens(&document);
+			let document: &str = value.unwrap();
+            let tokens = tokenize(&document);
             for tok in tokens {
-                println!("{}", tok);
+                *vocab.entry(tok).or_insert(0) += 1; 
+            } 
+            n_doc = n_doc + 1;
+            if n_doc % 500 == 0 {
+                println!("Building vocab...{} docs processed, vocab items={}", n_doc ,vocab.keys().len());
             }
         } true
     }).unwrap();
+
+    let mut counts: Vec<usize> = Vec::new();
+    for (_, &count) in vocab.iter() {
+        counts.push(count);
+    }
+    counts.sort();
+    counts.reverse();
+    let min_freq = counts[cmp::min(vocab_size, counts.len())];
+
+    let mut output = File::create(&vocab_file).unwrap();
+    let mut lines_written: usize = 0;
+    for (tok, &count) in vocab.iter() {
+        if &count >= &min_freq {
+            write!(output, "{} {}\n", tok, count).unwrap();
+            lines_written = lines_written + 1;
+        }
+        if lines_written >= vocab_size {
+            break
+        }
+    }
+    
+
 }
 /**
  let conn = db::open(&db_name).unwrap();
@@ -67,27 +160,27 @@ self.conn
 					let firstword: String = value.unwrap().to_string();
 					set_firstwords.insert(firstword);
 
-				} true
+				} true 
 		   }).unwrap();
  */
 fn main() {
 
-    let mut weights: HashMap<(&str, usize), Nexus> = HashMap::new();
-    let word = String::from("cat");
-    for i in 0..300 {
-        weights.insert( (&word, i), Nexus::new());
+ 
+    //shibboleth::build_vocab_from_db("wiki.db", "wikivocab.txt", 1000000, 25000);
+    let mut enc = Encoder::new(300, "delvocab.txt");
+    let p = enc.predict("forest", "sea");
+    match p {
+        Some(val) => println!("pred={}", val),
+        None => println!("key is missing")
     }
+    
 
-    let sentence = String::from("Today I walked slowly to the garden in San Diego.");
-    let tokens = nlp::text_tokens(&sentence);
-    for tok in tokens {
-        println!("{}", tok);
-    }
-    build_vocab_from_db("wiki.db","TBD",20);
-
-    let path = "lines.txt";
-
-    let mut output = File::create(path).unwrap();
-    write!(output, "Rust\n💖\nFun").unwrap();
     println!("Hello, world!");
+}
+
+#[test]
+fn test_tokenization(){
+    let tokens = tokenize("Totally! I love cupcakes!");
+    assert_eq!(tokens[0], "total");
+    assert_eq!(tokens[3], "cupcak");
 }
